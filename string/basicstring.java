5641544daf2987cd25488c93a9fe053b0bc42b15
jaygautam13 <gautamjay1304@gmail.com>
2026-03-08 23:46:23 +0530

    String first program

---
+++ string/basicstring.java
@@ -0,0 +1,15 @@
+package string;
+
+public class basicstring {
+
+    public static void main(String[] args) {
+
+        char[] arr = {'j','a','y'};
+        for (char c : arr) {
+            System.out.println(c);
+        }
+        
+
+    }
+
+}
