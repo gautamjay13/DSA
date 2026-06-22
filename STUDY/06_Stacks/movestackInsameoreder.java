2c4f6ed0bef73df09bd612973ecbe00e546f871d
jay gautam <jaygautamlu13@gmail.com>
2026-06-22 22:53:41 +0530

    stacks basic

---
+++ STUDY/06_Stacks/movestackInsameoreder.java
@@ -0,0 +1,33 @@
+
+
+import java.util.Stack;
+
+import java.util.Scanner;
+
+public class movestackInsameoreder {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> st = new Stack<>();
+        st.push(1) ;
+        st.push(2) ;
+        st.push(3) ;
+        st.push(4) ;
+        st.push(5) ;
+        System.out.println(st);
+        // reverse order 
+
+        Stack<Integer> gt = new Stack<>();
+        while (st.size()>0) {
+            gt.push(st.pop()) ;
+        }
+        System.out.println(gt);
+
+         Stack<Integer> rt = new Stack<>();
+        while (gt.size()>0) {
+           rt.push(gt.pop()) ;
+        }
+        System.out.println(rt);
+    }
+}
